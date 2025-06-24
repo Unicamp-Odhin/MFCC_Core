5534f15a5817a50f9aefe9b3aadd9ca5103ab795
Julio Nunes Avelar <jn5.guape@gmail.com>
2025-06-24 15:53:40 -0300

    Iniciando implementacão em C do MFCC

---
+++ lib/fft.h
@@ -0,0 +1,5 @@
+#ifndef __FFT_H__
+#define __FFT_H__
+
+
+#endif // __FFT_H__
---
+++ lib/wav.h
@@ -0,0 +1,28 @@
+#ifndef __WAV_H__
+#define __WAV_H__
+
+#include <stdint.h>
+
+typedef struct {
+    char chunkID[4];      // "RIFF"
+    uint32_t chunkSize;
+    char format[4];        // "WAVE"
+    
+    // Subchunk1 (fmt )
+    char subchunk1ID[4];   // "fmt "
+    uint32_t subchunk1Size;
+    uint16_t audioFormat;
+    uint16_t numChannels;
+    uint32_t sampleRate;
+    uint32_t byteRate;
+    uint16_t blockAlign;
+    uint16_t bitsPerSample;
+
+    // Subchunk2 (data)
+    char subchunk2ID[4];   // "data"
+    uint32_t subchunk2Size;
+} WavHeader;
+
+WavHeader * open_wav_file(const char *filename, int16_t **samples);
+
+#endif // __WAV_H__
