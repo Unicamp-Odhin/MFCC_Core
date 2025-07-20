9e2c71dd0954be80e08b12ac4928007ecd4c38e2
Vinicius P M Miguel <viniciusp070204@gmail.com>
2025-07-20 18:22:27 -0300

    q15_fft: add fuction fft_iterative

--- src/q15_fft.c
+++ src/q15_fft.c
@@ -1,4 +1,6 @@
 #include "q15.h"
+#include <inttypes.h>
+#include <stdio.h>
 #include "q15_fft.h"
 
 int32_t complex_power_q30(complex_q15 x) {
@@ -21,7 +23,9 @@ void generate_twiddles(complex_q15* twiddles, int N) {
     }
 }
 
-void fft_recursive(complex_q15* x, int N, complex_q15* twiddles) {
+
+
+void fft_recursive(complex_q15* x, int N, complex_q15* twiddles, int N_total) {
     if (N <= 1) return;
 
     int half = N / 2;
@@ -33,11 +37,12 @@ void fft_recursive(complex_q15* x, int N, complex_q15* twiddles) {
         odd[i]  = x[2 * i + 1];
     }
 
-    fft_recursive(even, half, twiddles);
-    fft_recursive(odd,  half, twiddles);
+    fft_recursive(even, half, twiddles, N_total);
+    fft_recursive(odd,  half, twiddles, N_total);
 
     for (int k = 0; k < half; k++) {
-        complex_q15 t = q15_complex_mul(twiddles[(k * (N / 2)) / N], odd[k]);
+        int twiddle_index = k * (N_total / N);  // Correção aqui
+        complex_q15 t = q15_complex_mul(twiddles[twiddle_index], odd[k]);
 
         x[k]        = q15_complex_add(even[k], t);
         x[k + half] = q15_complex_sub(even[k], t);
@@ -45,6 +50,44 @@ void fft_recursive(complex_q15* x, int N, complex_q15* twiddles) {
 
     free(even);
     free(odd);
+}
+
+void fft_iterative(complex_q15* x, int N, complex_q15* twiddles) {
+    int logN = 0;
+    for (int temp = N; temp > 1; temp >>= 1) logN++;
+
+    // Bit-reversal permutation
+    for (int i = 0, j = 0; i < N; i++) {
+        if (i < j) {
+            complex_q15 temp = x[i];
+            x[i] = x[j];
+            x[j] = temp;
+        }
+        int mask = N >> 1;
+        while (j & mask) {
+            j &= ~mask;
+            mask >>= 1;
+        }
+        j |= mask;
+    }
+
+    // Iterative FFT
+    for (int s = 1; s <= logN; s++) {
+        int m = 1 << s; // 2^s
+        int half_m = m >> 1;
+        int twiddle_step = N / m;
+
+        for (int k = 0; k < N; k += m) {
+            for (int j = 0; j < half_m; j++) {
+                int twiddle_index = j * twiddle_step;
+                complex_q15 t = q15_complex_mul(twiddles[twiddle_index], x[k + j + half_m]);
+                complex_q15 u = x[k + j];
+
+                x[k + j] = q15_complex_add(u, t);
+                x[k + j + half_m] = q15_complex_sub(u, t);
+            }
+        }
+    }
 }
 
 void fft_q15_real_power(q15_t* x_real, int N, int16_t* power_out) {
@@ -59,12 +102,14 @@ void fft_q15_real_power(q15_t* x_real, int N, int16_t* power_out) {
         x[i].imag = 0;
     }
 
+
     // 2. Gerar twiddles
     complex_q15* twiddles = malloc((NFFT / 2) * sizeof(complex_q15));
     generate_twiddles(twiddles, NFFT);
 
     // 3. Executar FFT
-    fft_recursive(x, NFFT, twiddles);
+    fft_iterative(x, NFFT, twiddles);
+    // fft_recursive(x, NFFT, twiddles, NFFT / 2);
 
     // 4. Calcular espectro de potência até N/2 (DC a Nyquist)
     int32_t temppower = 0;
@@ -75,7 +120,7 @@ void fft_q15_real_power(q15_t* x_real, int N, int16_t* power_out) {
         temppower = complex_power_q30(x[k]); // Q30
         
         temppower = temppower >> 15; // Q30->Q15
-
+        
         // Ajuste do ganho (*1/512 = >>9)
         temppower = temppower >> 9;  // Q15->Q6
 
@@ -85,6 +130,7 @@ void fft_q15_real_power(q15_t* x_real, int N, int16_t* power_out) {
 
         power_out[k] = (int16_t)temppower; // Guarda em Q6
     }
+    printf("\n");
 
     free(x);
     free(twiddles);
