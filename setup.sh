ec4682241cfaa406a55c3bd6419e7f63f826a90c
Vinicius P. M. Miguel <viniciusp070204@gmail.com>
2026-09-03 19:08:23 -0300

    update gitignore

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ TESTS_DIR="${ROOT_DIR}/tests"
 TABLES_DIR="${ROOT_DIR}/tables"
 WAV_DIR="${ROOT_DIR}/wav"
 SAMPLES_DIR="${ROOT_DIR}/samples"
-DPI_DIR="${ROOT_DIR}/dpi"
 REF_C_DIR="${ROOT_DIR}/ref_model/c"
 REF_PYTHON_DIR="${ROOT_DIR}/ref_model/python"
 BUILD_DIR="${ROOT_DIR}/build"
@@ -16,4 +15,4 @@ OBJ_DIR="${ROOT_DIR}/obj_dir"
 
 # Exporta variáveis para uso em outros scripts
 export PROJECT_ROOT="$ROOT_DIR"
-export RTL_DIR TESTS_DIR TABLES_DIR DPI_DIR REF_C_DIR REF_PYTHON_DIR BUILD_DIR OBJ_DIR
+export RTL_DIR TESTS_DIR TABLES_DIR WAV_DIR SAMPLES_DIR REF_C_DIR REF_PYTHON_DIR BUILD_DIR OBJ_DIR
